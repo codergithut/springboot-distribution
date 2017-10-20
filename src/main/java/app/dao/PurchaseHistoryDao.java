b66bc5e1823f24bcc4d5b30b56cf056ba4268381
tianjian <1731857742@qq.com>
2017-10-20 17:21:30 +0800

    提交

---
+++ src/main/java/app/dao/PurchaseHistoryDao.java
@@ -0,0 +1,9 @@
+package app.dao;
+
+/**
+ * @author <a href="mailto:Administrator@gtmap.cn">Administrator</a>
+ * @version 1.0, 2017/10/20
+ * @description
+ */
+public class PurchaseHistoryDao {
+}
